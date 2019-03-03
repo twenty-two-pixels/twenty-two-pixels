aa0e3a616f47a47854c63037a3fde41f489465cb
Ricky Clegg <rick@22px.io>
2019-03-03 21:33:17 +0000

    Add privacy and cookie routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const TITLE = 'Twenty Two Pixels';
+
+function createPageTitle(page) {
+  return `${TITLE} | ${page}`;
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { title: TITLE });
+});
+
+router.get('/cookiepolicy', function (req, res) {
+  res.render('cookie-policy', {title: createPageTitle('Cookie Policy')});
+});
+
+router.get('/privacypolicy', function (req, res) {
+  res.render('privacy-policy', {title: createPageTitle('Privacy Policy')});
 });
 
 module.exports = router;
